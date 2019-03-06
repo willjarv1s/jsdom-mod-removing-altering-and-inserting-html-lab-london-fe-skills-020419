6df6bc3dc50488341352dc24b91ca65ca6fdae71
Will Jarvis <will@toohotlimited.com>
2019-03-06 20:54:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerText = 'Will is the champion!'
